b5771ed4ee0b66c76a0680efaadbcd50988e6223
Je Yun <alwaysblue21@gmail.com>
2023-12-13 08:15:50 -0500

    params1

---
+++ params.js
@@ -0,0 +1,35 @@
+////////////////////////////////
+// Function Parameters
+///////////////////////////////
+// Parameters/Input --> Function --> Return Value/Output
+
+function addNums(){
+    console.log(1 + 1)
+}
+
+addNums()
+
+// x,y are parameters (placeholders)
+function addNums(x, y){
+    console.log(x + y)
+}
+
+// invoke --> pass arguments to fill those parameters
+addNums(2, 3)
+
+// x,y are parameters (placeholders)
+function addNums(x, y){
+    console.log(x + y)
+}
+
+// invoke --> pass arguments to fill those parameters
+addNums(2,3)
+addNums(3,3)
+addNums(5,6)
+
+// Write a function that takes 3 arguments, adds the first two and multiplies by the third and logs the result
+ function addAndMulti(x, y, z){
+    console.log((x + y) * z)
+ }
+
+ addAndMulti(1,2,3)
